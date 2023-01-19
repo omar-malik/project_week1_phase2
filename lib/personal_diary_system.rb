b0faa24e3bb3ba37c6af021de6c8ecb999bdf537
omar <omar-malik22@hotmail.com>
2023-01-19 14:31:59 +0000

    refactored after looking up ways/ using ai to refactor code

--- lib/personal_diary_system.rb
+++ lib/personal_diary_system.rb
@@ -1,10 +1,4 @@
 def make_snippet(string)
-  if string.split.length <= 5
   arr = string.split
-  return arr[0..4].join(" ")
-  elsif 
-    string.split.length > 5
-    arr = string.split
-    return arr[0..4].join(" ") + " ..."
-  end
+  return arr.first(5).join(" ") + (arr.length > 5 ? " ..." : "")
 end
