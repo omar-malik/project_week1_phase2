01241e85182fe670d1c903a6006668ba950d3c97
omar <omar-malik22@hotmail.com>
2023-01-19 14:26:31 +0000

    tested and written the 'make_snippet' method

--- lib/personal_diary_system.rb
+++ lib/personal_diary_system.rb
@@ -2,5 +2,9 @@ def make_snippet(string)
   if string.split.length <= 5
   arr = string.split
   return arr[0..4].join(" ")
+  elsif 
+    string.split.length > 5
+    arr = string.split
+    return arr[0..4].join(" ") + " ..."
   end
 end
--- spec/personal_diary_system_spec.rb
+++ spec/personal_diary_system_spec.rb
@@ -1,7 +1,16 @@
 require 'personal_diary_system'
 
 RSpec.describe "testing 'make_snippet' method" do
-  it "returns first 5 words if string is less than 5 words" do
+  it "returns first 5 words if string is < 5 words" do
   expect(make_snippet("Let's test it")).to eq "Let's test it"
+  end
+
+  it "returns first 5 words when string is == 5 words" do
+    expect(make_snippet("hi, how are you today?")).to eq "hi, how are you today?"
+  end
+
+  it "returns first 5 words + '...' when string is > 5 words" do
+    expect(make_snippet("Today is another day of the full time course and it's nice, 
+    the skies are clear and blue.")).to eq "Today is another day of ..."
   end
 end
